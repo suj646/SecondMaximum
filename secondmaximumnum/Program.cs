8b190cc4287ebfc2e87eca33508d329a9af08d72
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:14:59 +0530

    Second Maximum

---
+++ secondmaximumnum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace secondmaximumnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = { 2, 11, 15, 1, 7, 98, 3, 80, 4 };
+            Array.Sort(array);   
+            Array.Reverse(array);   
+            Console.WriteLine("Second max Value In Array " + array[1]);
+
+            foreach (var result in array)
+            {
+                Console.Write(result + " ");    
+            }
+        }
+    }
+}
